51091ee8e7994066bb14011e1e0024f044377d20
CoderGuy614 <jlutz.110@gmail.com>
2020-07-08 23:24:50 +0800

    Further work on Firebase Login

--- src/signin-signup/signup.js
+++ src/signin-signup/signup.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./sign-up-form.css";
 
-const signup = () => {
-  return (
-    <Form className="sign-up-form">
+import { auth, createUserProfileDocument } from "../firebase/firebaseUtils";
+
+const Signup = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    redirect: false,
+  });
+
+  const { email, password, error, loading, redirect } = values;
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+    console.log(values);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+  };
+
+  const signUpForm = () => (
+    <Form className="sign-up-form" onSubmit={handleSubmit}>
       <Form.Group controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          value={values.email}
+          onChange={handleChange("email")}
+        />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -16,7 +49,12 @@ const signup = () => {
 
       <Form.Group controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          value={values.password}
+          onChange={handleChange("password")}
+        />
       </Form.Group>
       <Form.Group controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
@@ -26,6 +64,8 @@ const signup = () => {
       </Button>
     </Form>
   );
+
+  return <div className="container">{signUpForm()}</div>;
 };
 
-export default signup;
+export default Signup;
