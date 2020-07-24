deefc482d6afe5007f9af302845359cca5c4e667
CoderGuy614 <jlutz.110@gmail.com>
2020-07-24 18:13:31 +0800

    Experimenting with Grid Layouts on Homepage

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,9 +1,39 @@
 import React from "react";
 
+import "./homepage.css";
+
 const Homepage = () => {
   return (
-    <div className="homepage">
-      <h2>I AM A HOME PAGE</h2>
+    <div className="container">
+      <div className="grid">
+        <div className="homepage-main-content">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae a
+          dignissimos doloremque eum voluptate! Alias voluptatibus facere
+          voluptate, doloremque vitae mollitia beatae, dolore quod quas enim
+          consequuntur consectetur unde, odio placeat labore vero veritatis
+          voluptates perspiciatis. Veritatis temporibus ut, cupiditate suscipit,
+          provident magni accusamus reiciendis ea laboriosam explicabo rem
+          molestiae.
+        </div>
+        <div className="homepage-secondary-content">
+          {" "}
+          Secondary Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Minima debitis eligendi blanditiis ea doloribus laboriosam, corrupti
+          commodi, perspiciatis et natus adipisci in consequuntur ratione
+          distinctio neque id consectetur cumque culpa odit numquam voluptate
+          ipsum sint quos eveniet. Sapiente sit, sed numquam fuga corporis porro
+          accusantium aliquid similique asperiores, rem explicabo?
+        </div>
+        <div className="homepage-sidebar">
+          {" "}
+          Sidebar Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Recusandae, totam ab veritatis eveniet quas expedita nihil at, rerum
+          ut libero id, exercitationem quis officia distinctio perspiciatis
+          incidunt alias ipsa facilis a quo culpa odit? Deleniti aspernatur
+          necessitatibus natus corrupti, enim quas quo quisquam magnam laborum
+          atque aut quos voluptatibus fugit.
+        </div>
+      </div>
     </div>
   );
 };
