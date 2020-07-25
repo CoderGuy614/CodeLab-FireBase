985b03a92181b1ce5cee7f0f39960349f9977b68
CoderGuy614 <jlutz.110@gmail.com>
2020-07-25 12:17:37 +0800

    Testing and Experimenting with Firestore

--- src/components/post/PostForm.js
+++ src/components/post/PostForm.js
@@ -2,58 +2,42 @@ import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { setAlert } from "../../redux/actions/alert";
 import Message from "../message/Message";
-import { postMessage, getMessages } from "../../firebase/firebaseUtils";
-
+// import { postMessage, getMessages } from "../../firebase/firebaseUtils";
+import { firestore } from "../../firebase/firebaseUtils";
 import "./post-form.css";
 
 const PostForm = ({ setAlert }) => {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
 
-  const handleMessages = () => {
-    let messageArray = [];
-    getMessages()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => messageArray.push(doc.data()));
-        setMessages(messageArray);
-      })
-      .catch((err) => console.log(err));
-  };
-
-  useEffect(() => {
-    handleMessages();
-  }, []);
-
   const handleChange = (e) => {
     setMessage(e.target.value);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (message) {
-      postMessage(message)
-        .then((docRef) => {
-          setAlert(`Message Posted with ID ${docRef.id}`, "success");
-          console.log("YES ITS WORKING");
-          setMessage("");
-        })
-        .catch((error) => setAlert(`Something went wrong ${error}`));
-    }
+  const postMessage = () => {
+    firestore
+      .collection("messages")
+      .add({
+        message,
+      })
+      .then(function (docRef) {
+        console.log(docRef.id);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
 
   return (
     <>
       <div className="post-form">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Enter a message"
-            onChange={handleChange}
-            value={message}
-          />
-          <button>Post Message</button>
-          <button onClick={() => handleMessages()}>Get Messages</button>
-        </form>
+        <input
+          type="text"
+          placeholder="Enter a message"
+          onChange={handleChange}
+          value={message}
+        />
+        <button onClick={postMessage}>Post Message</button>
       </div>
       <div className="message-display">
         {messages.map((msg, i) => (
