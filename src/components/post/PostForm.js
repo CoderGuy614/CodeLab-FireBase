ac0169b97ee5fc9ac35e8750ff75cfcefa5d5736
CoderGuy614 <jlutz.110@gmail.com>
2020-07-25 15:32:36 +0800

    Modify PostForm Set and Get Message Functions

--- src/components/post/PostForm.js
+++ src/components/post/PostForm.js
@@ -2,42 +2,64 @@ import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { setAlert } from "../../redux/actions/alert";
 import Message from "../message/Message";
-// import { postMessage, getMessages } from "../../firebase/firebaseUtils";
+import Spinner from "../loading/Spinner";
 import { firestore } from "../../firebase/firebaseUtils";
 import "./post-form.css";
 
 const PostForm = ({ setAlert }) => {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
     setMessage(e.target.value);
   };
 
-  const postMessage = () => {
+  const postMessage = (e) => {
+    e.preventDefault();
+    setLoading(true);
     firestore
       .collection("messages")
       .add({
         message,
       })
       .then(function (docRef) {
-        console.log(docRef.id);
+        setAlert(`Message Posted with ID of ${docRef.id}`, "success");
+        setLoading(false);
+        setMessage("");
       })
       .catch(function (error) {
-        console.log(error);
+        setLoading(false);
+        setAlert(`Something went wrong: ${error}`, "danger");
       });
   };
+
+  const getMessagesRealtime = () => {
+    firestore.collection("messages").onSnapshot((querySnapshot) => {
+      let currentMessages = [];
+      querySnapshot.forEach((doc) => currentMessages.push(doc.data()));
+      setMessages(currentMessages);
+    });
+  };
+
+  getMessagesRealtime();
 
   return (
     <>
       <div className="post-form">
-        <input
-          type="text"
-          placeholder="Enter a message"
-          onChange={handleChange}
-          value={message}
-        />
-        <button onClick={postMessage}>Post Message</button>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <form onSubmit={postMessage}>
+            <input
+              type="text"
+              placeholder="Enter a message"
+              onChange={handleChange}
+              value={message}
+            />
+            <button type="submit">Post Message</button>
+          </form>
+        )}
       </div>
       <div className="message-display">
         {messages.map((msg, i) => (
