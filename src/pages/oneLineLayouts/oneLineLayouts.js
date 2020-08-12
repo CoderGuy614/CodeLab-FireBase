795fa893c0dfcaff4facb15a34bb065d77971ad1
CoderGuy614 <jlutz.110@gmail.com>
2020-08-12 20:12:20 +0800

    Add content to one line layout section

--- src/pages/oneLineLayouts/oneLineLayouts.js
+++ src/pages/oneLineLayouts/oneLineLayouts.js
@@ -9,7 +9,7 @@ const superCentered = () => (
       </div>
     </div>
     <div className="description">
-      <h3>This is SuperCentered</h3>
+      <h3>SuperCentered</h3>
       <code>{`.parent {
   display: grid;
   place-items: center;
@@ -30,7 +30,7 @@ const deconstructedPancake = () => (
     </div>
 
     <div className="description">
-      <h3>This is Deconstructed Pancake</h3>
+      <h3>Deconstructed Pancake</h3>
       <code>{`  .ex2 .parent {
     display: flex;
     flex-wrap: wrap;
@@ -47,11 +47,34 @@ const deconstructedPancake = () => (
   </div>
 );
 
+const sidebarSays = () => (
+  <div className="ex3">
+    <div className="parent">
+      <div className="section yellow">Sidebar</div>
+      <div className="section purple">Main</div>
+    </div>
+
+    <div className="description">
+      <h3>Sidebar Says</h3>
+      <code>{`  .ex3 .parent {
+    display: grid;
+    grid-template-columns: minmax(150px, 25%) 1fr;
+  }`}</code>
+    </div>
+  </div>
+);
+
 const OneLineLayouts = () => {
   return (
-    <div id="onelinelayouts" className="container">
+    <div id="onelinelayouts">
+      {
+        <h3 className="text-center mb-4 text-primary text-underline">
+          <u>Handy Layout Options Using 1 Line of CSS</u>
+        </h3>
+      }
       {superCentered()}
       {deconstructedPancake()}
+      {sidebarSays()}
     </div>
   );
 };
