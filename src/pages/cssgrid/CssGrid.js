d1b8ff508549ef29eda34b2f31799f66116f5862
CoderGuy614 <jlutz.110@gmail.com>
2020-08-14 12:38:25 +0800

    Create CSS Grid Layouts

--- src/pages/cssgrid/CssGrid.js
+++ src/pages/cssgrid/CssGrid.js
@@ -1,19 +1,53 @@
 import React from "react";
 import "./cssGrid.scss";
+import { Container } from "react-bootstrap";
 
 const CssGrid = () => {
   return (
-    <div className="ex1">
-      <h3 className="text-center mt-3">Basic Grid Layout</h3>
-      <div className="container">
-        <div className="item item1"></div>
-        <div className="item item2"></div>
-        <div className="item item3"></div>
-        <div className="item item4"></div>
-        <div className="item item5"></div>
-        <div className="item item6"></div>
+    <Container>
+      <div className="ex1">
+        <h3 className="text-center mt-3">Basic Grid Layout</h3>
+        <div className="container">
+          <div className="item item1">Item 1</div>
+          <div className="item item2">Item 2</div>
+          <div className="item item3">Item 3</div>
+          <div className="item item4">Item 4</div>
+          <div className="item item5">Item 5</div>
+          <div className="item item6">Item 6</div>
+        </div>
       </div>
-    </div>
+      <div className="ex2">
+        <h3 className="text-center mt-3">Grid Template Rows</h3>
+        <div className="container">
+          <div className="item item1"> Row 1</div>
+          <div className="item item2">Row 2</div>
+          <div className="item item3">Row 3</div>
+          <div className="item item4"> Row 4</div>
+        </div>
+      </div>
+
+      <div className="ex3">
+        <h3 className="text-center mt-3">Grid Template Columns</h3>
+        <div className="container">
+          <div className="item item1"> Column 1</div>
+          <div className="item item2">Column 2</div>
+          <div className="item item3">Column 3</div>
+          <div className="item item4"> Column 4</div>
+        </div>
+      </div>
+
+      <div className="ex4">
+        <h3 className="text-center mt-3">Grid Template Rows & Columns</h3>
+        <div className="container">
+          <div className="item item1">Row 1</div>
+          <div className="item item2">3Fr height</div>
+          <div className="item item2">3 Columns</div>
+          <div className="item item1">Row 2</div>
+          <div className="item item2">1Fr height</div>
+          <div className="item item2">Row 3 Columns</div>
+        </div>
+      </div>
+    </Container>
   );
 };
 
